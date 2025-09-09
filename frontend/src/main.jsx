49ae2324ebd3e82acff9b0b02b48bf6f295b8313
Gregg Marayan <101786033+GreggMarayan@users.noreply.github.com>
2025-09-09 23:25:00 +0800

    Add admin dashboard and booking pages with routing
    Introduces AdminDashboard and AdminBooking components and sets up React Router for navigation between main, admin, and booking pages in main.jsx.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
+import AdminDashboard from './pages/admin/adminDashboard.jsx'
+import AdminBooking from './pages/admin/AdminBooking.jsx'
+import './index.css'
+
+const router = createBrowserRouter([
+  {path: "/", element: <App />},
+  {path: "/admin", element: <AdminDashboard />},
+  {path: "/booking", element: <AdminBooking />}
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
